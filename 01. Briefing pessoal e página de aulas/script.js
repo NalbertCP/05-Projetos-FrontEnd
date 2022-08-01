f852ce20d491a9c52eef26fc94a946d7d9165f75
Nalbert Cerqueira <105606295+NalbertCP@users.noreply.github.com>
2022-08-01 20:25:43 -0300

    Update script.js

--- 01. Briefing pessoal e página de aulas/script.js
+++ 01. Briefing pessoal e página de aulas/script.js
@@ -1,36 +1,26 @@
-const modaloverlay = document.querySelector(".modal_overlay")
+const modaloverlay = document.querySelector(".modal-overlay")
 const cardsimg = document.querySelectorAll(".card img")
 const closemodal = document.querySelector(".material-icons")
 const cards = document.querySelectorAll(".card")
 
-/* ATIVAR O MODAL */
 for (let cardimg of cardsimg){
-    cardimg.addEventListener("click", function(){
-        modaloverlay.classList.add("active")
-        closemodal.classList.add("material-icons")
-        modaloverlay.querySelector("iframe").src=`https://www.youtube.com/embed/${cardimg.id}`
+    cardimg.addEventListener("click", ()=>{
+        showmodal(cardimg)
     })   
 }
-
-/* ATIVAR E DESATIVAS A FEATURED*/
-for (let card of cards){
-    card.addEventListener("mouseover",()=>{
-        card.classList.add("card_featured")
-        let featured = card.querySelector(".featured")
-        featured.innerHTML = "Featured"
-        featured.classList.add("active")
-    })
-    card.addEventListener("mouseleave",()=>{
-        let featured = card.querySelector(".featured")
-        card.classList.remove("card_featured")
-        featured.innerHTML = ""
-        featured.classList.remove("active")
-    })
-}
-
-/* DESATIVAR O MODAL */
-closemodal.addEventListener("click", function(){
-    modaloverlay.classList.remove("active")
-    closemodal.classList.remove("material-icons")
-    modaloverlay.querySelector("iframe").src=""
+modaloverlay.addEventListener("click",()=>{
+    hidemodal(event)
 })
+
+function showmodal (cardimg){
+    modaloverlay.classList.add("active")
+    closemodal.classList.add("material-icons")
+    modaloverlay.querySelector("iframe").src=`https://www.youtube.com/embed/${cardimg.id}`
+}
+function hidemodal (event){
+    if (event.target.className==="material-icons" || event.target.className==="modal-overlay active"){
+        modaloverlay.classList.remove("active")
+        closemodal.classList.remove("material-icons")
+        modaloverlay.querySelector("iframe").src=""
+    }
+}
