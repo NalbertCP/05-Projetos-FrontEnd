d1b5afd835303186b571858428f7b1326c8ffa60
Nalbert Cerqueira <105606295+NalbertCP@users.noreply.github.com>
2022-09-13 00:57:52 -0300

    Add files via upload

---
+++ 06. Lista de contatos/public/js/check-inputs.js
@@ -0,0 +1,16 @@
+//Selecioando todos os inputs dados do formulário
+const formInputs = document.querySelectorAll("form .input")
+
+//Modificado o aviso de preenchimento do campo para cada input
+for (let input of formInputs){
+    input.addEventListener("invalid",invalidInput)
+    input.addEventListener("change",validInput)
+}
+
+//Funções
+function invalidInput(){
+    this.setCustomValidity("Por favor, preencha o campo ou digite um valor válido")
+}
+function validInput(){
+    this.setCustomValidity("")
+}
---
+++ 06. Lista de contatos/public/js/modal-delete.js
@@ -0,0 +1,19 @@
+//Selecionando os elementos html
+const modal = document.querySelector(".modal-overlay")
+const formDelete = document.querySelector(".form-delete")
+
+//Ativando o modal após o submit do form
+formDelete.onsubmit = function(event){
+    event.preventDefault()
+    modal.classList.remove("modal-hidden")   
+}
+
+//Veriricando o cancelamento ou confirmação da delação ao interagir com o modal
+modal.onclick = function(event){
+    let target = event.target
+    if (target.className==="modal-delete"){
+        formDelete.submit()
+    } else if (target.className === "material-icons" || target.className === "cancel-delete"){
+        modal.classList.add("modal-hidden")
+    }
+}
