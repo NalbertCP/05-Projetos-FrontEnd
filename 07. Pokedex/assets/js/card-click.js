f0724e032e29251b8735b2f10f0c7e5446841bb0
Nalbert Cerqueira <105606295+NalbertCP@users.noreply.github.com>
2022-09-25 16:20:05 -0300

    Update card-click.js

--- 07. Pokedex/assets/js/card-click.js
+++ 07. Pokedex/assets/js/card-click.js
@@ -1,9 +1,28 @@
-/*Escondendo as estatísticas dos card ao clicar fora dele*/
-let cardClicked = ""
-window.addEventListener("click",(event)=>{
-    if (event.target.name==="card-click"){
-        cardClicked = event.target
-    } else{
-        cardClicked.checked = false
-    }
+/*Declarando variáveis*/
+let cardTarget = ""
+let form = document.querySelector("form")
+
+/*Cancelando o submit do form de que contém os filtros*/
+form.addEventListener("submit",(event)=>{
+    event.preventDefault()
 })
+
+/*Criando efeito de animação do card ao clica-lo*/
+window.addEventListener("click",animateCard)
+
+/*Funções*/
+function animateCard(event){
+    cardTarget? setCardAsUnclicked() : ""
+    event.target.className === "card"? setCardAsClicked(event) : ""
+}
+function setCardAsClicked(event){
+    cardTarget = event.target
+    cardTarget.querySelector(".card-img").classList.add("move-img")
+    cardTarget.querySelector(".card-info").classList.add("move-info")
+    cardTarget.querySelector(".more-info").classList.add("show")
+}
+function setCardAsUnclicked(){
+    cardTarget.querySelector(".card-img").classList.remove("move-img")
+    cardTarget.querySelector(".card-info").classList.remove("move-info")
+    cardTarget.querySelector(".more-info").classList.remove("show")
+}
