5f0e2b45dd71236586cffa93324abe363abd2369
Nalbert Cerqueira <105606295+NalbertCP@users.noreply.github.com>
2022-09-27 15:32:36 -0300

    Update move-label.js

--- 05. Landing Page - Netflix/assets/js/move-label.js
+++ 05. Landing Page - Netflix/assets/js/move-label.js
@@ -1,11 +1,9 @@
 /*Selecionando elementos do DOM*/
 const emailInputs = document.querySelectorAll("input[type=email]")
-const emailLabels = document.querySelectorAll("form > label")
 
 /*Movendo a label para o topo do input quando input.value!=""*/
-
 for (let input of emailInputs){
-    input.addEventListener("keyup",(event)=>{
+    input.addEventListener("keyup", (event)=>{
         input.value ? event.target.nextElementSibling.classList.add("label-goup"): 
         event.target.nextElementSibling.classList.remove("label-goup")
     })
