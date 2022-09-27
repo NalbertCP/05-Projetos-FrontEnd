2a7886b87140cd3a6835a7c68adba7f63332fb4b
Nalbert Cerqueira <105606295+NalbertCP@users.noreply.github.com>
2022-09-27 15:46:52 -0300

    Update routes.js

--- 06. Lista de contatos/controller/routes.js
+++ 06. Lista de contatos/controller/routes.js
@@ -4,17 +4,17 @@ const methods = require("./methods")
 const routes = express.Router()
 
 //Declaração das rotas utilizadas em server.js
-routes.get("/",(req,res)=>{
+routes.get("/", (req, res)=>{
     res.redirect("/contacts")
 })
-routes.get("/contacts",methods.index)
-routes.get("/contacts/search",methods.searchContact)
-routes.get("/contacts/create",methods.createContact)
-routes.get("/contacts/:id",methods.get)
-routes.get("/contacts/:id/edit",methods.editContact)
-routes.post("/contacts",methods.post)
-routes.put("/contacts",methods.put)
-routes.delete("/contacts",methods.deleteContact)
+routes.get("/contacts", methods.index)
+routes.get("/contacts/search", methods.searchContact)
+routes.get("/contacts/create", methods.createContact)
+routes.get("/contacts/:id", methods.get)
+routes.get("/contacts/:id/edit", methods.editContact)
+routes.post("/contacts", methods.post)
+routes.put("/contacts", methods.put)
+routes.delete("/contacts", methods.deleteContact)
 
 //Exportando as rotas para serem utilizadas em server.js
 module.exports = routes
