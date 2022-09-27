a232938ca05d690cd66dd6e143daaaaca7502939
Nalbert Cerqueira <105606295+NalbertCP@users.noreply.github.com>
2022-09-27 13:55:14 -0300

    Update close-modal.js

--- 03. Lista de tarefas/js/close-modal.js
+++ 03. Lista de tarefas/js/close-modal.js
@@ -3,8 +3,8 @@ const modal = document.querySelector(".modal-overlay")
 const textInput = document.querySelector("#text-input")
 const createTaskBtn = document.querySelector(".create-task")
 
-modal.addEventListener("click",closeModal)
-createTaskBtn.addEventListener("click",activateModal)
+modal.addEventListener("click", closeModal)
+createTaskBtn.addEventListener("click", activateModal)
 
 /*Fechando o modal ao clicar em "Fechar" ou no "X"*/
 function closeModal (event){
@@ -15,6 +15,5 @@ function closeModal (event){
 }
 /*Ativando o modal caso input.value esteja vazio*/
 function activateModal (){
-    if (textInput.value === "")
-    modal.classList.add("active")
+    if (textInput.value === "") modal.classList.add("active")
 }
