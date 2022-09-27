64ab6f25ab24245c1df8e7e0621775cee3c71515
Nalbert Cerqueira <105606295+NalbertCP@users.noreply.github.com>
2022-09-27 13:56:28 -0300

    Update input-label.js

--- 03. Lista de tarefas/js/input-label.js
+++ 03. Lista de tarefas/js/input-label.js
@@ -2,7 +2,7 @@
 const taskInput = document.querySelector("#text-input")
 const inputLabel = document.querySelector("#text-input + label")
 
-taskInput.addEventListener("keyup",()=>{
+taskInput.addEventListener("input", ()=>{
     /* Se input.value estiver vazio, a label volta para baixo, caso contrário, permanece em cima do input*/
     taskInput.value? inputLabel.className="move-label": inputLabel.className=""
 })
