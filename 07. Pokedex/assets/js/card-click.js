bc8c18e93a2963c368592eeff995447fd64cfb1d
Nalbert Cerqueira <105606295+NalbertCP@users.noreply.github.com>
2022-10-04 22:55:25 -0300

    Add files via upload

---
+++ 07. Pokedex/assets/js/card-click.js
@@ -0,0 +1,28 @@
+/*Declarando variáveis*/
+var cardTarget = "";
+var form = document.querySelector("form");
+
+/*Cancelando o submit do form que contém os filtros*/
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+});
+
+/*Criando efeito de animação do card ao clica-lo*/
+window.addEventListener("click", animateCard);
+
+/*Funções*/
+function animateCard(event) {
+  cardTarget ? setCardAsUnclicked() : "";
+  event.target.classList[1] === "card" ? setCardAsClicked(event) : "";
+}
+function setCardAsClicked(event) {
+  cardTarget = event.target;
+  cardTarget.querySelector(".card-img").classList.add("move-img");
+  cardTarget.querySelector(".card-info").classList.add("move-info");
+  cardTarget.querySelector(".more-info").classList.add("show");
+}
+function setCardAsUnclicked() {
+  cardTarget.querySelector(".card-img").classList.remove("move-img");
+  cardTarget.querySelector(".card-info").classList.remove("move-info");
+  cardTarget.querySelector(".more-info").classList.remove("show");
+}
---
+++ 07. Pokedex/assets/js/mvc/model.js
@@ -0,0 +1,18 @@
+export default class PokemonsData{
+    constructor(url){
+        this.url = url
+    }
+    
+    async requestData(){
+        this.pokemons = await (await fetch(this.url)).json()
+        this.createPokemonTypes()
+    }
+
+    createPokemonTypes(){
+        let types = []
+        for (let {type} of this.pokemons){
+            types.push(...type)
+        }
+        this.pokemonTypes = ["Todos", ...new Set(types)]
+    }
+}
