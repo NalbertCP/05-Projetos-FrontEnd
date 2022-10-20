5072e574845848c77bd645a122a0b2412ca0e268
Nalbert Cerqueira <105606295+NalbertCP@users.noreply.github.com>
2022-10-20 19:27:02 -0300

    Update check-inputs.js

--- 06. Lista de contatos/public/js/check-inputs.js
+++ 06. Lista de contatos/public/js/check-inputs.js
@@ -1,5 +1,7 @@
 //Selecioando todos os inputs do formulário
 const formInputs = document.querySelectorAll("form .input")
+const tellInput = document.querySelector("#number")
+const stateInput = document.querySelector("#state")
 
 //Modificado o aviso de preenchimento do campo para cada input
 for (let input of formInputs){
@@ -7,10 +9,29 @@ for (let input of formInputs){
     input.addEventListener("change", validInput)
 }
 
+tellInput.addEventListener("input", fromatPhoneInput)
+stateInput.addEventListener("input", formatStateInput)
+
 //Funções
 function invalidInput(){
     this.setCustomValidity("Por favor, preencha o campo ou digite um valor válido")
 }
 function validInput(){
     this.setCustomValidity("")
+}
+function fromatPhoneInput(event){
+    let length = tellInput.value.length
+    tellInput.value = tellInput.value.replace(/\D/g, "").replace(/(\d{2})/,"($1) ")
+    tellInput.value = tellInput.value.replace(/(\d{5})/,"$1-")
+
+    if (event.inputType === "deleteContentBackward"){
+        const lastCaracter = tellInput.value[length-1]
+        length === 5 || length === 4 ? tellInput.value = tellInput.value.replace(/\D/g, "") : null
+        try{
+            lastCaracter.match(/\D/) ? tellInput.value = tellInput.value.replace(lastCaracter,""): null
+        }catch(error){}
+    }
+}
+function formatStateInput(){
+    stateInput.value = stateInput.value.replace(/[^a-zA-Z]/g, "").toUpperCase()
 }
