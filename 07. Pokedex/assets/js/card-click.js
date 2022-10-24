584e925f1b89e5e27caa66e07bda4034a7e8fadf
Nalbert Cerqueira <105606295+NalbertCP@users.noreply.github.com>
2022-10-23 22:43:31 -0300

    Update card-click.js

--- 07. Pokedex/assets/js/card-click.js
+++ 07. Pokedex/assets/js/card-click.js
@@ -1,28 +1,28 @@
 /*Declarando variáveis*/
-var cardTarget = "";
-var form = document.querySelector("form");
+var cardTarget = ""
+var form = document.querySelector("form")
 
 /*Cancelando o submit do form que contém os filtros*/
 form.addEventListener("submit", function (event) {
-  event.preventDefault();
+  event.preventDefault()
 });
 
 /*Criando efeito de animação do card ao clica-lo*/
-window.addEventListener("click", animateCard);
+window.addEventListener("click", animateCard)
 
 /*Funções*/
 function animateCard(event) {
-  cardTarget ? setCardAsUnclicked() : "";
-  event.target.classList[1] === "card" ? setCardAsClicked(event) : "";
+  cardTarget ? setCardAsUnclicked() : ""
+  event.target.classList.contains("card") ? setCardAsClicked(event) : ""
 }
 function setCardAsClicked(event) {
-  cardTarget = event.target;
-  cardTarget.querySelector(".card-img").classList.add("move-img");
-  cardTarget.querySelector(".card-info").classList.add("move-info");
-  cardTarget.querySelector(".more-info").classList.add("show");
+  cardTarget = event.target
+  cardTarget.querySelector(".card-img").classList.add("move-img")
+  cardTarget.querySelector(".card-info").classList.add("move-info")
+  cardTarget.querySelector(".more-info").classList.add("show")
 }
 function setCardAsUnclicked() {
-  cardTarget.querySelector(".card-img").classList.remove("move-img");
-  cardTarget.querySelector(".card-info").classList.remove("move-info");
-  cardTarget.querySelector(".more-info").classList.remove("show");
+  cardTarget.querySelector(".card-img").classList.remove("move-img")
+  cardTarget.querySelector(".card-info").classList.remove("move-info")
+  cardTarget.querySelector(".more-info").classList.remove("show")
 }
