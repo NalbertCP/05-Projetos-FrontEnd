31a96f24cc8cc77a26957420668374eba286b5d1
Nalbert Cerqueira <nalbertc.p@gmail.com>
2022-11-21 20:31:56 -0300

    fixing prettier errors

--- 05. Landing Page - Netflix/assets/js/close-faq.js
+++ 05. Landing Page - Netflix/assets/js/close-faq.js
@@ -6,28 +6,28 @@ let targetParent = ""
 let targetParentId = ""
 
 /*Adicionado Event listener ao container de FAQs*/
-faqsContainer.addEventListener("click", (event)=>{
+faqsContainer.addEventListener("click", (event) => {
     targetParent = event.target.parentElement
     targetParentId = targetParent.id
-    if (targetParent.className === "card"){
+    if (targetParent.className === "card") {
         closeLastOpenedFAQ()
         toggleFAQ(event.target)
-    } 
+    }
 })
 
 /*Funções utilizadas*/
-function closeLastOpenedFAQ(){
-    for (let faqHeader of document.querySelectorAll(".open-card-button")){
+function closeLastOpenedFAQ() {
+    for (let faqHeader of document.querySelectorAll(".open-card-button")) {
         const id = faqHeader.parentElement.id
-        if (faqHeader.classList.contains("active") && id!=targetParentId) closeFAQ(faqHeader)
+        if (faqHeader.classList.contains("active") && id != targetParentId) closeFAQ(faqHeader)
     }
 }
-function toggleFAQ(faqHeader){
+function toggleFAQ(faqHeader) {
     faqHeader.classList.toggle("active")
     faqHeader.querySelector(".material-icons").classList.toggle("rotate")
     faqHeader.nextElementSibling.classList.toggle("content-hidden")
 }
-function closeFAQ(faqHeader){
+function closeFAQ(faqHeader) {
     faqHeader.classList.remove("active")
     faqHeader.querySelector(".material-icons").classList.remove("rotate")
     faqHeader.nextElementSibling.classList.add("content-hidden")
