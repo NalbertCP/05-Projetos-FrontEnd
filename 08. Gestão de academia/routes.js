d81480d27ca82802a6ef49c8fe0795f1ac785085
Nalbert Cerqueira <nalbertc.p@gmail.com>
2022-11-21 20:17:43 -0300

    fixing prettier errors

--- 08. Gestão de academia/routes.js
+++ 08. Gestão de academia/routes.js
@@ -4,13 +4,13 @@ const instructors = require("./controllers/instructors")
 const members = require("./controllers/members")
 const routes = express.Router()
 
-routes.get("/data", (req, res)=>{
+routes.get("/data", (req, res) => {
     res.setHeader("Cache-Control", "private, no-cache")
     res.sendFile(__dirname + "/data.json")
 })
 
 /*Rotas de acesso às funcionalidades dos instrutores*/
-routes.get("/", (req, res)=>res.redirect("/instructors"))
+routes.get("/", (req, res) => res.redirect("/instructors"))
 routes.get("/instructors", instructors.index)
 routes.get("/instructors/create", instructors.create)
 routes.get("/instructors/:id", instructors.findInstructor)
