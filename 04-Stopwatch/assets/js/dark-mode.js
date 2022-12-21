6461b4c0a5c320680975b3a47e59175cd7802330
Nalbert Cerqueira <105606295+nalbertcerqueira@users.noreply.github.com>
2022-12-21 01:24:08 -0300

    Update dark-mode.js

--- 04-Stopwatch/assets/js/dark-mode.js
+++ 04-Stopwatch/assets/js/dark-mode.js
@@ -9,7 +9,7 @@ const watchDisplay = document.querySelector(".watch")
 switchModeButton.onclick = switchMode
 window.onload = function () {
     const mode = localStorage.getItem("theme")
-    //Aplicando o thema do cronômetro ao inicar a aplicação com base no localStorage
+    //Aplicando o tema do cronômetro ao inicar a aplicação com base no localStorage
     switch (mode) {
         case "dark":
             switchModeButton.checked = true
@@ -32,18 +32,18 @@ function switchMode(event) {
 }
 function applyMode(checked) {
     if (checked) {
-        switchModeIcon.innerHTML = "light_mode" //Alerando ícone do botão de tema
+        switchModeIcon.innerHTML = "light_mode" //Alterando o ícone do botão de tema
         for (let simbol of simbols) {
             simbol.classList.add("white")
         }
         documentBody.classList.add("dark-mode") //Alterando as cores settadas no body
-        watchDisplay.classList.add("dark-mode") //Acrescentandodo animação light no display do relógio
+        watchDisplay.classList.add("dark-mode") //Acrescentando animação de luzes no display do relógio
     } else {
-        switchModeIcon.innerHTML = "dark_mode" //Alerando ícone do botão de tema
+        switchModeIcon.innerHTML = "dark_mode" //Alterando o ícone do botão de tema
         for (let simbol of simbols) {
             simbol.classList.remove("white")
         }
         documentBody.classList.remove("dark-mode") //Alterando as cores settadas no body
-        watchDisplay.classList.remove("dark-mode") //Acrescentandodo animação light no display do relógio
+        watchDisplay.classList.remove("dark-mode") //Acrescentando animação de luzes no display do relógio
     }
 }
