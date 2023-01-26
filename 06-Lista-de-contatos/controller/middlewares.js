9ad649868f9e6b1b784c9d2387039f4d39f8d260
Nalbert Cerqueira <nalbertc.p@gmail.com>
2023-01-26 16:56:27 -0300

    exported middlewares to a middleware file and added a middleware to update contacts age only one time per day

---
+++ 06-Lista-de-contatos/controller/middlewares.js
@@ -0,0 +1,20 @@
+const { cookieParser, updateAge } = require("./utils")
+const data = require("../data.json")
+
+//Handler para páginas inválidas
+function invalidRoutes(req, res) {
+    res.setHeader("Content-Type", "text/plain")
+    return res.status(404).send("Error 404.\nWe could not found the page you're looking for")
+}
+
+//Atualizando a data de aniversário dos contatos uma vez ao dia
+async function shouldUpdateAges(req, res, next) {
+    const agesUpdated = cookieParser(req.headers.cookie).ages_updated
+    if (!agesUpdated) {
+        res.cookie("ages_updated", "true", { maxAge: 86400000 })
+        await updateAge(res, data)
+    }
+    next()
+}
+
+module.exports = { invalidRoutes, shouldUpdateAges }
--- 06-Lista-de-contatos/controller/routes.js
+++ 06-Lista-de-contatos/controller/routes.js
@@ -1,13 +1,14 @@
 //Importando dependências e variáveis
 const express = require("express")
 const methods = require("./methods")
+const { invalidRoutes, shouldUpdateAges } = require("./middlewares")
 const routes = express.Router()
 
 //Declaração das rotas utilizadas em server.js
 routes.get("/", (req, res) => {
     res.redirect("/contacts")
 })
-routes.get("/contacts", methods.index)
+routes.get("/contacts", shouldUpdateAges, methods.index)
 routes.get("/contacts/new", methods.createContact)
 routes.get("/contacts/:id", methods.get)
 routes.get("/contacts/:id/edition", methods.editContact)
@@ -16,10 +17,7 @@ routes.put("/contacts", methods.put)
 routes.delete("/contacts", methods.deleteContact)
 
 //middleware handler para rotas inválidas
-routes.use((req, res) => {
-    res.setHeader("Content-Type", "text/plain")
-    return res.status(404).send("Error 404.\nWe could not found the page you're looking for")
-})
+routes.use(invalidRoutes)
 
 //Exportando as rotas para serem utilizadas em server.js
 module.exports = routes
