7bcef078633b4fff708fd593e6426d5064d43bb6
Nalbert Cerqueira <105606295+nalbertcerqueira@users.noreply.github.com>
2023-03-17 01:29:27 -0300

    Delete 03-Lista-de-tarefas directory

--- 03-Lista-de-tarefas/assets/js/input-label.js
+++
@@ -1,8 +0,0 @@
-/*Variáveis gerais*/
-const taskInput = document.querySelector("#text-input")
-const inputLabel = document.querySelector("#text-input + label")
-
-taskInput.addEventListener("keyup", () => {
-    //Se input.value estiver vazio, a label volta para baixo, caso contrário, permanece em cima do input
-    inputLabel.className = taskInput.value ? "move-label" : ""
-})
