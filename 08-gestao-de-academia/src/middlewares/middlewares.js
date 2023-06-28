bac69ee69d2af4927d874e1de9fe6d2820e7a8ac
nalbert-cerqueira <nalbertc.p@gmail.com>
2023-06-28 20:38:27 -0300

    changed the language in api error messages, from 'en' to 'pt-br'.

--- 08-gestao-de-academia/src/middlewares/middlewares.js
+++ 08-gestao-de-academia/src/middlewares/middlewares.js
@@ -6,8 +6,8 @@ require("dotenv").config()
 
 module.exports.invalidRoutes = function (req, res) {
     res.status(404).render("./errors.njk", {
-        status: "Error 404",
-        msg: "We could not find the page you're looking for."
+        status: "Erro 404",
+        msg: "Desculpe, não conseguimos encontrar a página."
     })
 }
 
